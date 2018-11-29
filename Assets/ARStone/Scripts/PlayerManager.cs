6c743962ce10b29f351cd8d789fd573102808f8b
jptrindade <joao_pedro_trindade@hotmail.com>
2018-11-29 15:46:36 +0000

    Player life bar and prototype for dragon bar

--- Assets/ARStone/Scripts/PlayerManager.cs
+++ Assets/ARStone/Scripts/PlayerManager.cs
@@ -6,19 +6,39 @@ using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour {
     private int Life;
+    private float currentPercentage;
+    public Image Bar;
     public Text life_text;
+
+    public int MaxHP;
+
 	// Use this for initialization
 	void Start () {
-        Life = 30;
-        life_text.text = "Player Life: " + Life.ToString();
+        Life = MaxHP;
+        life_text.text = "100%";
+	currentPercentage = 1;
+	Bar.fillAmount = currentPercentage;
 	}
 
     public void DecreaseLife(int delta)
     {
         Life -= delta;
-        life_text.text = "Player Life: " + Life.ToString();
+	if(Life <= 0){
+		Life = 0;
+		currentPercentage = 0;
+
+	} 
+        else{
+		currentPercentage = (float)Life/MaxHP;
+	}
+
+        life_text.text = string.Format("{0} %", Mathf.RoundToInt(currentPercentage * 100));
+	Bar.fillAmount = currentPercentage;
     }
 
+    public float GetPercentage(){
+	return currentPercentage;
+    }
     public int GetLife()
     {
         return Life;
