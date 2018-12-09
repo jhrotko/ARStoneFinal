cd8ada6ccad1a4a91e8e31b5b292e480410aaeef
D3ath22 <pedro.monteiro22@gmail.com>
2018-12-09 21:13:39 +0000

    FIxed FCT

--- Assets/FCT.cs
+++ Assets/FCT.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class FCT : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject DragonText;
+    public GameObject ZombieText;
+    public GameObject DruidText;
+    float time = 0.0f;
+
+
+    // Use this for initialization
+    void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        //Move();
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= 3.5f)
+        {
+            ZombieText.GetComponent<TextMesh>().text = "";
+            ZombieText.GetComponent<TextMesh>().text = "";
+            DruidText.GetComponent<TextMesh>().text = "";
+            DruidText.GetComponent<TextMesh>().text = "";
+            DragonText.GetComponent<TextMesh>().text = "";
+            DragonText.GetComponent<TextMesh>().text = "";
+        }
     }
-
     private void Move()
     {
         transform.Translate(Vector2.up * 1 * Time.deltaTime);
@@ -25,22 +39,24 @@ public class FCT : MonoBehaviour {
         if (damage >= 0)
         {
             pm.gameObject.transform.GetChild(0).gameObject.GetComponent<TextMesh>().text = damage.ToString();
-        }
+            time = 0;
+        }/*
         else
         {
             pm.gameObject.transform.GetChild(1).gameObject.GetComponent<TextMesh>().text = damage.ToString();
-        }               
+        }*/          
     }
 
     public void SetPosition(GameObject pm, int damage)
     {
         if (damage >= 0)
         {
-            pm.transform.GetChild(4).gameObject.GetComponent<TextMesh>().text = damage.ToString();
-        }
+           pm.transform.GetChild(4).gameObject.GetComponent<TextMesh>().text = damage.ToString();
+           time = 0;
+        }/*
         else
         {
             pm.transform.GetChild(5).gameObject.GetComponent<TextMesh>().text = damage.ToString();
-        }
+        }*/
     }
 }
